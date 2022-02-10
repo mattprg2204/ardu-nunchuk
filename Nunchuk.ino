f680e189be0317d2d9b0681bd6cfcd299efbcc98
mattprg2204 <53111076+mattprg2204@users.noreply.github.com>
2022-02-10 16:51:32 +0100

    Beispielimplementierung hinzugefügt

---
+++ Nunchuk.ino
@@ -0,0 +1,25 @@
+#include "Nunchuk.h"
+
+using namespace communication;
+
+I2CBus bus{ Control::ADDR_NUNCHUK };
+
+void setup() {
+  // Seriellen Monitor für Debug Initialisieren
+  Serial.begin(9600);
+
+  // I2C-Bus Initialisieren
+  Wire.begin();
+}
+
+void loop() {
+  // Falls Verbindung Verloren: erneut verbinden
+  if (!bus.isConnected())
+    bus.begin();
+
+  // Messwerte auslesen
+  bus.read();
+
+  // Ausgabe der Messwerte
+  // bus.getDevice().print();
+}
